4644e04d212623b59e794eca91be3f1302f1708c
Fmr6917 <90112774+Fmr6917@users.noreply.github.com>
2026-02-15 01:36:39 +0400

    Door Animation + UI
    - Door Opening and Closing Animation with script
    - Welcome UI Panel
    - Play Button
    - Tutorial Button
    - Settings Button
    - Back to login Button

---
+++ Assets/Scripts/DoorAnimatorToggle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimatorToggle : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        ToggleDoorOpen();
+        Invoke("ToggleDoorclose", 5f);
+    }
+
+   public void ToggleDoorOpen()
+    {
+        anim.SetBool("DoorOpen",true);
+    }
+
+     public void ToggleDoorclose()
+    {
+        anim.SetBool("DoorOpen",false);
+    }
+}
